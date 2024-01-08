1bc6dda9605090e23cbedd47ca3d18a7adf6df7c
jezernik <58013872+jezernik@users.noreply.github.com>
2024-01-08 14:18:41 +0100

    use module SG

--- main.tf
+++ main.tf
@@ -26,14 +26,29 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog.sg.security_group_id]
 }
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "blog_new"
+  
+   vpc_id=data.aws_vpc.default.id
+
+   ingress_rules = ["http-80-tcp","https-443-tcp"]
+   ingress_cidr_blocks=["0.0.0.0/0"]
+   egress_rules = ["all-all"]
+}
+
 
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "allow http and https in; allow all out"
   vpc_id=data.aws_vpc.default.id
 }
+
+
 
 resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
