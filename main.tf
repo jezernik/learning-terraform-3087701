56ed5aeb063efe2dbf9c8cb5b10b236219a7b019
jezernik <58013872+jezernik@users.noreply.github.com>
2024-01-08 12:39:41 +0100

    security groups defined

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -21,4 +25,44 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "HelloWorld"
   }
+
+  vpc_security_group_ids = [aws_security_group.blog.id]
+}
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "allow http and https in; allow all out"
+  vpc_ids=aws_security_group.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks=["0.0.0.0/0"]
+
+  security_group_ids=aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks=["0.0.0.0/0"]
+
+  security_group_ids=aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_http_out" {
+type = "egress"
+
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks=["0.0.0.0/0"]
+
+  security_group_id=aws_security_group.blog.id
+
 }
